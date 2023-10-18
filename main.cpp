74a2eeef79c83b7aac3c78278de9697774cb9c82
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-10-18 16:42:28 +0900

    基本的に最大・最小のグループで要素間移動するように修正 (16086269)

--- main.cpp
+++ main.cpp
@@ -30,10 +30,10 @@ map<char,char> ch_r = {{'<','>'},{'>','<'},{'=','='}};
 
 struct State {
     char ch;
-    int n, q, d;
+    int n, q, d, cnt;
 
     State() : n(0) {}
-    explicit State(int _n, int _q, int _d) : n(_n), q(_q), d(_d) {}
+    explicit State(int _n, int _q, int _d) : n(_n), q(_q), d(_d), cnt(0) {}
     
     inline void relationUpdate(const int e1, const int e2, vector<vector<char>>& r) {
         // ~~~ 個々の関係性の更新 ~~~ //
@@ -59,27 +59,47 @@ struct State {
         cout << '\n' << flush, cin >> ch;
         r[i1][i2] = ch;
         r[i2][i1] = ch_r[ch];
+        return;
+    }
 
-        // 3段論法の要領で a < b && b < c なら a < c も情報追加 (worshal-froid風)
-        for(int k=0; k<d; k++) for(int i=0; i<d; i++) for(int j=0; j<d; j++) {
-            if( r[i][k] != '?' && r[i][k] == r[k][j] ) r[i][j] = r[i][k];
-            if( r[j][k] != '?' && r[j][k] == r[k][i] ) r[j][i] = r[j][k];
+    inline void groupCompare(int ele, vector<int>& rank, vector<vector<char>>& r, vector<vector<int>>& latest) {
+        // ※ rank は降順で並んでいるとする
+        if( rank.empty() ) {
+            rank.emplace_back(ele);
+            return;
+        }
+
+        int top = 0, under = rank.size();
+        while( under-top > 1 ) {
+            int mid = (top+under)/2;
+            if( !q ) return;
+            relationUpdate(ele, rank[mid], r, latest);
+            if( r[ele][rank[mid]] == '<' ) top = mid+1;
+            else if( r[ele][rank[mid]] == '>' ) under = mid;
+            else {
+                top = mid;
+                break;
+            }
+        }
+        if( top != rank.size() ) {
+            if( !q ) return;
+            relationUpdate(ele, rank[top], r, latest);
+        }
+        if( top != rank.size() && r[ele][rank[top]] == '<' ) top++;
+        rank.insert(rank.begin() + top, ele);
+        for(int i=0; i<rank.size(); i++) {
+            if( i < top ) r[ele][rank[i]] = '<', r[rank[i]][ele] = '>';
+            if( i > top ) r[ele][rank[i]] = '>', r[rank[i]][ele] = '<';
         }
         return;
     }
 
-    inline void smoothing(const int g1, const int g2, vector<vector<char>>& r_one, vector<vector<char>>& r_group, vector<int>& res, vector<vector<int>>& latest) {
+    inline void smoothing(const int g1, const int g2, vector<vector<char>>& r_one, vector<vector<char>>& r_group, vector<int>& res, vector<vector<int>>& latest, vector<int>& rank) {
         int i1 = rand_int()%latest[g1].size(), i2 = rand_int()%latest[g2].size();
         int e1 = latest[g1][i1], e2 = latest[g2][i2];
 
         if( r_one[e1][e2] == '?' ) relationUpdate(e1, e2, r_one);
-        if( r_one[e1][e2] == '=' || !q ) return;
-
-        if( r_group[g1][g2] == '?' ) relationUpdate(g1, g2, r_group, latest);
-        if( r_group[g1][g2] == '=' || !q ) return;
-
-        // 軽 ⇒ 重 に重い荷物を渡す時は return (大小関係が崩れる為)
-        if( r_group[g1][g2] == ch_r[r_one[e1][e2]] ) return;
+        if( r_one[e1][e2] == '=' || r_one[e1][e2] == '<' || !q ) return;
 
         swap(latest[g1][i1], latest[g2][i2]);
         cout << latest[g1].size() << " " << latest[g2].size() << " ";
@@ -88,21 +108,26 @@ struct State {
         cout << '\n' << flush, cin >> ch;
         if( !(--q) ) return;
 
-        if( r_group[g1][g2] != ch ) swap(latest[g1][i1], latest[g2][i2]);
-        else swap(res[e1],res[e2]);
+        if( r_group[g1][g2] != ch ) {
+            swap(latest[g1][i1], latest[g2][i2]);
+            cnt++;
+        }
+        else {
+            cnt = 0;
+            swap(res[e1],res[e2]);
+            vector<int> next;
+            for(int i=0; i<d; i++) if( rank[i] != g1 && rank[i] != g2 ) next.emplace_back(rank[i]);
+            swap(rank, next);
+            groupCompare(g1, rank, r_group, latest);
+            groupCompare(g2, rank, r_group, latest);
+        }
         return;
     }
 
-    inline void moving(const int g1, int g2, vector<vector<char>>& r_group, vector<int>& res, vector<vector<int>>& latest) {
+    inline void moving(const int g1, int g2, vector<vector<char>>& r_group, vector<int>& res, vector<vector<int>>& latest, vector<int>& rank) {
         if( latest[g1].size() == 1 ) return;
         int i1 = rand_int()%latest[g1].size();
         int e1 = latest[g1][i1];
-
-        if( r_group[g1][g2] == '?' ) relationUpdate(g1, g2, r_group, latest);
-        if( r_group[g1][g2] == '=' || !q ) return;
-
-        // 軽 ⇒ 重 に重い荷物を移動する時は return (大小関係が崩れる為)
-        if( r_group[g1][g2] == '<' ) return;
 
         latest[g2].emplace_back(e1);
         latest[g1].erase(latest[g1].begin()+i1);
@@ -116,8 +141,17 @@ struct State {
         if( r_group[g1][g2] != ch ) {
             latest[g1].emplace_back(e1);
             latest[g2].pop_back();
+            cnt++;
         }
-        else res[e1] = g2;
+        else {
+            cnt = 0;
+            res[e1] = g2;
+            vector<int> next;
+            for(int i=0; i<d; i++) if( rank[i] != g1 && rank[i] != g2 ) next.emplace_back(rank[i]);
+            swap(rank, next);
+            groupCompare(g1, rank, r_group, latest);
+            groupCompare(g2, rank, r_group, latest);
+        }
         return;
     }
 };
@@ -125,7 +159,7 @@ struct State {
 struct Solver {
     char ch;
     int n, d, q;
-    vector<int> res;
+    vector<int> res, g_rank;
 
     State state;
     vector<vector<int>> latest;
@@ -138,6 +172,7 @@ struct Solver {
         relation_group.assign(d,vector<char>(d,'?'));
         relation_one.assign(n,vector<char>(n,'?'));
         latest.assign(d,vector<int>{});
+        g_rank.assign({});
         res.assign(n,0);
         for(int i=0; i<d; i++) relation_group[i][i] = '=';
         for(int i=0; i<n; i++) relation_one[i][i] = '=';
@@ -149,30 +184,43 @@ struct Solver {
         while( state.q-- ) cout << 1 << " " << 1 << " " << 0 << " " << 1 << '\n' << flush, cin >> ch;
         for(int i=0; i<n; i++) cout << res[i] << " ";
         cout << flush << endl;
+        for(auto ele:g_rank) cerr << ele << " ";
+        cerr << endl;
         return;
     }
 
     void solve(){
         utility::mytm.CodeStart();
 
-        // 1. 適当にgroup作成
+        // 1. group作成 & groupの大小管理
         for(int i=0; i<n; i++) {
             res[i] = i%d;
             latest[res[i]].emplace_back(i);
         }
+        for(int i=0; i<d; i++) state.groupCompare(i, g_rank, relation_group, latest);
 
         // 2. group間の関係を調整
         while( state.q && utility::mytm.elapsed() <= TIME_LIMIT ) {
             // feature : 完全乱択ではなく、
             //           原始モンテカルロで期待値を求める ⇒ 差が大きい group を候補にして要素swap
-            int g1 = rand_int()%d, g2 = rand_int()%d;
-            while( g1 == g2 ) g2 = rand_int()%d;
+            int g1 = g_rank[0], g2 = g_rank.back();
+            if( state.cnt >= 5 ) {
+                g1 = rand_int()%d, g2 = g1;
+                while( g1 == g2 ) g2 = rand_int()%d;
+                for(int i=0; i<d; i++) {
+                    if( g_rank[i] == g1 ) break;
+                    if( g_rank[i] == g2 ) {
+                        swap(g1,g2);
+                        break;
+                    }
+                }
+            }
 
             int query = rand_int()%2;
-            if( query ) state.smoothing(g1, g2, relation_one, relation_group, res, latest);
-            else state.moving(g1, g2, relation_group, res, latest);
+            if( query ) state.smoothing(g1, g2, relation_one, relation_group, res, latest, g_rank);
+            else state.moving(g1, g2, relation_group, res, latest, g_rank);
         }
-        cerr << "Rest Query Time: " << q << endl;
+        cerr << "Rest Query Time: " << state.q << endl;
         return;
     }
 };
